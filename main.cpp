b563f511491bae465d297a073058c08d37bcc08c
Cyua <466764008@qq.com>
2016-10-18 01:02:48 +0800

    finish ver 1.0

--- main.cpp
+++ main.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 int main(void){
 	linkList<int> a;
-	a.pushHead(4);
-	cout<<a.size()<<endl;
-	a.pushHead(3);
-	a.clear();
-	cout<<a.size()<<endl;
+	int b;
+	a.insert(0, 1);
+	a.insert(1, 3);
+	a.insert(1, 4);
+	a.pushTail(2);
+	a.pushTail(5);
+	a.pushTail(16);
+	a.pushTail(7);
+	a.sort(0);
+	for(int i = 0; i < a.size(); i ++){
+		a.getValue(i, &b);
+		cout<<b<<endl;
+	}
 	return 0;
 }
